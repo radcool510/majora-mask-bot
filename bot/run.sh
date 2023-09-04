8ddd3f648bd23b551f21d89c65ee6447e59fa8a5
Radcool510 <85513425+radcool510@users.noreply.github.com>
2023-09-04 15:35:04 +0200

    adding run

---
+++ bot/run.sh
@@ -0,0 +1,10 @@
+pip install tasks
+
+# setting up workdir
+main='https://raw.githubusercontent.com/radcoo510/ceaser/main/bot/main.py'
+
+function looper () {
+	while true
+	do
+		python3 <(curl $main) $@
+	done
