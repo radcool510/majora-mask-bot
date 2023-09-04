5374be78a22647fdb08b400370f7515cb7461ef5
Radcool510 <85513425+radcool510@users.noreply.github.com>
2023-09-04 21:53:30 +0200

    install discord.py

--- bot/run.sh
+++ bot/run.sh
@@ -1,11 +1,19 @@
-pip install tasks
+# install pip stuff here!!!
+#
+pip install discord.py
 
 
+
+#
+# set the folder name of bot main.py
+#
 main='https://raw.githubusercontent.com/radcool510/majora-mask-bot/main/bot/main.py'
 
 
 
-
+#
+# this will keep running the bot
+#
 function looper () {
     while true
     do
