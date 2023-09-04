058c3d67d300b8270981a43163e3702c44a8f791
Radcool510 <85513425+radcool510@users.noreply.github.com>
2023-09-04 21:11:27 +0200

    Update run.sh

--- bot/run.sh
+++ bot/run.sh
@@ -2,7 +2,7 @@ pip install tasks
 pip install openai
 
 # setting up workdir
-main='https://raw.githubusercontent.com/radcoo510/majora_mask_bot/main/bot/main.py'
+main='https://raw.githubusercontent.com/radcoo510/majora-mask-bot/main/bot/main.py'
 
 function looper () {
 	while true
