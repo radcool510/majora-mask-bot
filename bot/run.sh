209b26dc7aef0e7df451a38a0c69a98d205aa629
Radcool510 <85513425+radcool510@users.noreply.github.com>
2023-09-04 21:51:08 +0200

    put away pip install openai

--- bot/run.sh
+++ bot/run.sh
@@ -1,14 +1,11 @@
 pip install tasks
-pip install openai
 
 
 main='https://raw.githubusercontent.com/radcool510/majora-mask-bot/main/bot/main.py'
 
 
 
-#
-# this will keep running the bot
-#
+
 function looper () {
     while true
     do
